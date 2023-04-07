5ffb35e3901331df20037151edccb79b183c967f
Mahmoud Akram Al-Mallahi <nalmamoud303@gmail.com>
2023-04-07 18:28:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/clothingsuggester/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.clothingsuggester.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.clothingsuggester.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
